6d0d7a2fecef7d87bc49e4ace30cd3eceec13b4b
someshwarduggirala <someshwarduggirala@gmail.com>
2020-01-16 03:39:38 +0000

    hangman completed by Someshwar Duggirala

--- hangman/game.py
+++ hangman/game.py
@@ -1,24 +1,67 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ["rmotr","python","code"]
 
 
 def _get_random_word(list_of_words):
-    pass
-
+    if list_of_words:
+        return random.choice(list_of_words)
+    
+    raise InvalidListOfWordsException ()
 
 def _mask_word(word):
-    pass
+    if word:
+        return '*'*len(word)
+    raise InvalidWordException()
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    
+    if not answer_word or not masked_word or not character:
+        raise InvalidWordException()
+        
+    if len(character) > 1:
+        raise InvalidGuessedLetterException()
+    
+    if len(masked_word)!=len(answer_word):
+        raise InvalidWordException()
+    
+    num_match=0
+    character=character.lower()
+    
+    for index,char in enumerate(answer_word.lower()):
+        if char==character:
+            num_match+=1
+            masked_word=masked_word[:index]+char+masked_word[index+1:]
+    
+    return masked_word
+    
+        
 
 
 def guess_letter(game, letter):
-    pass
-
+    
+    
+    if game['remaining_misses']==0 or game['masked_word'] == game['answer_word'].lower():
+        raise GameFinishedException()
+    
+    new_masked_word=_uncover_word(game['answer_word'],game['masked_word'],letter)
+    
+    if new_masked_word == game['masked_word']:
+        game['remaining_misses']-=1
+        
+    game['masked_word']=new_masked_word
+    game['previous_guesses'].append(letter.lower())
+    
+    if game['masked_word'] == game['answer_word'].lower():
+        raise GameWonException()
+    
+    if game['remaining_misses']==0:
+        raise GameLostException()
+    
+    
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
     if list_of_words is None:
